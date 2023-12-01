20fdaf7b053c0e99f759ccb9ae9db16d5f644c54
Aishan <121613744+AhmedAishan@users.noreply.github.com>
2023-12-01 08:56:57 +0530

    Open reg form

--- script.js
+++ script.js
@@ -2,10 +2,40 @@
 
 console.log('Project Elevate started');
 
-// DATA
-// skeleton
-// sign-up form
-// sticky nav
-// smooth scrolling
-// reveal sections
-// lazy loading
+// 0- DATA
+const openSignUp = document.querySelector('.btn-signup');
+const registration = document.querySelector('.registration');
+const closeSignUp = document.querySelector('.registration__btn');
+const overlay = document.querySelector('.overlay');
+
+const hidden = document.querySelector('.hidden');
+
+// 1- sign-up form
+
+const openRegistrationForm = function (e) {
+  e.preventDefault();
+
+  registration.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeRegistrationForm = function (e) {
+  registration.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+openSignUp.addEventListener('click', openRegistrationForm);
+closeSignUp.addEventListener('click', closeRegistrationForm);
+overlay.addEventListener('click', closeRegistrationForm);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !registration.classList.contains('hidden'))
+    closeRegistrationForm();
+});
+
+// account created message to the user
+
+// 2- smooth scrolling
+// 3- sticky nav
+// 4- reveal sections
+// 5- lazy loading
+// 7- skeleton
